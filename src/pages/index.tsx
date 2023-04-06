4bc0ef6eed6107c4b8743d8c3b25d8ebe923c1d4
developerbrm <developerbrm@gmail.com>
2023-04-07 00:52:09 +0530

    - "daisyUI" added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,25 @@ export default function Home() {
         <h1 className="center select2-dropdown p-3 text-5xl text-amber-500 shadow-xl">
           Just Lorem things!
         </h1>
+
+        <div className="alert alert-success shadow-lg">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 flex-shrink-0 stroke-current"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>Your purchase has been confirmed!</span>
+          </div>
+        </div>
       </main>
     </>
   )
